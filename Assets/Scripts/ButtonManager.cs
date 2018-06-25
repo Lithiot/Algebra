af6f9728edb092dba5422250f5f21d9063c26761
Unknown <agustingarcia1997@gmail.com>
2018-06-25 17:19:05 -0300

    Replay de rotacion arreglada

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour {
 
     [SerializeField] private GameObject Dice;
+    private TransformRotation rot;
+
+    private void Awake()
+    {
+        rot = Dice.GetComponent<TransformRotation>();
+    }
 
     public void RestarDice()
     {
+        
         TransformPosition.instanciate.Position = Vector3.zero;
-        TransformRotation.instanciate.Rotation = Vector3.zero;
+        Dice.transform.rotation = Quaternion.identity;
         TransformScale.instanciate.scale = Vector3.one;
+        rot.Reinicio = true;
     }
 }
