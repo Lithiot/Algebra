26b0ffa45aa46262ca21e0c1f45d2c3f928d70fe
Unknown <lean1998@hotmail.com>
2018-07-10 12:53:05 -0300

    fixes

---
+++ Assets/Scripts/AplicationQuit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AplicationQuit : MonoBehaviour {
+
+    public void Close()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
